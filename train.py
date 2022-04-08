6b9b7945d1b9e43dfe2cafbdb648e81ef7b3c4cb
tongxingyu <tongxingyu@ln.chinamobile.com>
2022-04-08 10:31:40 +0800

    fix random bug.

--- train.py
+++ train.py
@@ -4,18 +4,18 @@ import time
 import glob
 import datetime
 import argparse
-import random
 
 import numpy as np
 import paddle
-import paddle.nn as nn
 from paddle.io import DataLoader
 import cv2
 from PIL import Image
 
 from arch_unet import UNet
-from augment_noise import AugmentNoise
+# from augment_noise import AugmentNoise
 from dataset import DataLoader_Imagenet_val
+
+# 验收标准：Gaussion 25, BSD300: PSNR: 30.79, SSIM:0.873
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--noisetype", type=str, default="gauss25")
@@ -42,9 +42,81 @@ systime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
 operation_seed_counter = 0
 os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_devices
 
-paddle.seed(1)
-np.random.seed(1)
-random.seed(1)
+
+class AugmentNoise(object):
+    def __init__(self, style):
+        print(style)
+        if style.startswith('gauss'):
+            self.params = [
+                float(p) / 255.0 for p in style.replace('gauss', '').split('_')
+            ]
+            if len(self.params) == 1:
+                self.style = "gauss_fix"
+            elif len(self.params) == 2:
+                self.style = "gauss_range"
+        elif style.startswith('poisson'):
+            self.params = [
+                float(p) for p in style.replace('poisson', '').split('_')
+            ]
+            if len(self.params) == 1:
+                self.style = "poisson_fix"
+            elif len(self.params) == 2:
+                self.style = "poisson_range"
+
+    def add_train_noise(self, x):
+        shape = x.shape
+        if self.style == "gauss_fix":
+            std = self.params[0]
+            # std = std * torch.ones((shape[0], 1, 1, 1), device=x.device)
+            std = std * paddle.ones((shape[0], 1, 1, 1))
+            # noise = torch.cuda.FloatTensor(shape, device=x.device)
+            # torch.normal(mean=0.0,
+            #              std=std,
+            #              generator=get_generator(),
+            #              out=noise)
+            global operation_seed_counter
+            operation_seed_counter += 1
+            paddle.seed(operation_seed_counter)
+            noise = paddle.normal(mean=0.0, std=std)
+
+            return x + noise
+        # elif self.style == "gauss_range":
+        #     min_std, max_std = self.params
+        #     std = torch.rand(size=(shape[0], 1, 1, 1),
+        #                      device=x.device) * (max_std - min_std) + min_std
+        #     noise = torch.cuda.FloatTensor(shape, device=x.device)
+        #     torch.normal(mean=0, std=std, generator=get_generator(), out=noise)
+        #     return x + noise
+        # elif self.style == "poisson_fix":
+        #     lam = self.params[0]
+        #     lam = lam * paddle.ones((shape[0], 1, 1, 1))
+        #     noised = torch.poisson(lam * x, generator=get_generator()) / lam
+        #     return noised
+        # elif self.style == "poisson_range":
+        #     min_lam, max_lam = self.params
+        #     lam = torch.rand(size=(shape[0], 1, 1, 1),
+        #                      device=x.device) * (max_lam - min_lam) + min_lam
+        #     noised = torch.poisson(lam * x, generator=get_generator()) / lam
+        #     return noised
+
+    def add_valid_noise(self, x):
+        shape = x.shape
+        if self.style == "gauss_fix":
+            std = self.params[0]
+            return np.array(x + np.random.normal(size=shape) * std,
+                            dtype=np.float32)
+        elif self.style == "gauss_range":
+            min_std, max_std = self.params
+            std = np.random.uniform(low=min_std, high=max_std, size=(1, 1, 1))
+            return np.array(x + np.random.normal(size=shape) * std,
+                            dtype=np.float32)
+        elif self.style == "poisson_fix":
+            lam = self.params[0]
+            return np.array(np.random.poisson(lam * x) / lam, dtype=np.float32)
+        elif self.style == "poisson_range":
+            min_lam, max_lam = self.params
+            lam = np.random.uniform(low=min_lam, high=max_lam, size=(1, 1, 1))
+            return np.array(np.random.poisson(lam * x) / lam, dtype=np.float32)
 
 
 def checkpoint(net, epoch, name):
@@ -75,6 +147,9 @@ def generate_mask_pair(img):
 
     idx_pair = paddle.to_tensor(
         np.array([[0, 1], [0, 2], [1, 3], [2, 3], [1, 0], [2, 0], [3, 1], [3, 2]]).astype('int64'))
+    global operation_seed_counter
+    operation_seed_counter += 1
+    paddle.seed(operation_seed_counter)
     rd_idx = paddle.randint(low=0,
                             high=8,
                             shape=(n * h // 2 * w // 2,))
